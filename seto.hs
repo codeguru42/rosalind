c1daf9c7684858f7881cbf88e2546755d0ba29a7
codeguru <codeguru@users.sourceforge.net>
2013-12-18 07:04:27 -0700

    SETO: Implement parser and use built-in set operations

--- seto.hs
+++ seto.hs
@@ -4,6 +4,38 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Control.Applicative ((*>), (<$>))
+import Data.Either (rights)
+import Data.List (union, intersect, (\\))
+import Text.ParserCombinators.Parsec
+import Text.Parsec.Token
+
+set = do
+    char '{'
+    result <- list
+    char '}'
+    return result
+
+list = sepBy1 cell (char ',')
+cell = many space *> many (noneOf ",}")
+
+parseSet :: String -> Either ParseError [String]
+parseSet = parse set "(unknown)"
+
+showSet s = '{' : showList s ++ "}"
+    where showList [x] = show x
+          showList (x:xs) = show x ++ ", " ++ showList xs 
+
 main = do
     contents <- readFile "seto.txt"
-    putStr contents
+    let ls = lines contents
+    let n = read $ ls !! 0 :: Int
+    let sets = map (map read) $ rights . map parseSet $ drop 1 ls :: [[Int]]
+    let a = sets !! 0
+    let b = sets !! 1
+    putStrLn $ showSet (a `union` b)
+    putStrLn $ showSet (a `intersect` b)
+    putStrLn $ showSet (a \\ b)
+    putStrLn $ showSet (b \\ a)
+    putStrLn $ showSet ([1..n] \\ a)
+    putStrLn $ showSet ([1..n] \\ b)
