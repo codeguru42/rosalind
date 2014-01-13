38106bfdd3b37aeefd8a4657beaff94989bad222
codeguru <codeguru@users.sourceforge.net>
2014-01-12 20:34:10 -0700

    Implement `countSignedPermutations`

--- sign.hs
+++ sign.hs
@@ -4,7 +4,14 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Rosalind
 import Data.List (permutations)
 
-main = print $ permutations [1..n]
+countSignedPermutations :: Integer -> Integer
+countSignedPermutations n = 2 ^ n * product [1..n]
+
+main = do
+    let perms = permutations [1..n]
+    print $ countSignedPermutations n
+    mapM_ putStrLn $ map format perms
     where n = 2
