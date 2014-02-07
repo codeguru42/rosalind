cd36f5d3b2c571aecee1d9268823444b90c464f4
codeguru <codeguru@users.sourceforge.net>
2014-02-07 12:41:04 -0700

    SPLC: Implement `splice` in terms of `exons`; fix indexing issues

--- splc.hs
+++ splc.hs
@@ -4,13 +4,24 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Data.List (sortBy)
+
 import Rosalind
+
+splice dna = concat . exons dna
+
+exons dna introns = exons' dna intronIndexes 0
+    where is = map (flip indexes dna) introns
+          intronIndexes = sortBy (\x y -> fst x `compare` fst y) . concat $ zipWith (\x y -> zip x . repeat $ length y) is introns
+          exons' ""  [] _ = []
+          exons' dna [] _ = [dna]
+          exons' dna ((x, y):es) n = take (x - n) dna : exons' (drop (x - n + y) dna) es (x + y)
 
 main = do
     contents <- readFile "splc.txt"
     let fasta = parse contents
     let dnas = snd $ unzip fasta
     let dna = head dnas
-    let exons = tail dnas
-    let is = map (flip indexes dna) exons
-    mapM_ print is
+    let introns = tail dnas
+    let splc = splice dna introns
+    putStrLn . protein $ transcribe splc
