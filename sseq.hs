df033d49ae9f139b1b7cbe10fc1053bc194ac245
codeguru <codeguru@users.sourceforge.net>
2014-02-07 14:59:22 -0700

    SSEQ: Final solution

--- sseq.hs
+++ sseq.hs
@@ -4,10 +4,19 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Data.List (elemIndex, tails)
+import Data.Maybe (fromMaybe)
+
 import Rosalind
+
+spliceSequence :: String -> String -> [Int]
+spliceSequence dna1 dna2 = spliceSequence' dna1 dna2 0
+    where spliceSequence' _ [] _ = []
+          spliceSequence' dna1 (d:ds) n = n + k : spliceSequence' (drop (k + 1) dna1) ds (n + k + 1)
+            where Just k = d `elemIndex` dna1   
 
 main = do
     contents <- readFile "sseq.txt"
     let fasta = parse contents
     let dnas = snd $ unzip fasta
-    mapM_ putStrLn dnas
+    putStrLn . format . map (+1) $ spliceSequence (dnas !! 0) (dnas !! 1)
