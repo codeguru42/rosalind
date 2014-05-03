0ec7fb295cba3e01c726814694e9265b7ab6a3e6
codeguru <codeguru@users.sourceforge.net>
2014-05-02 23:42:18 -0600

    KMER: Implement `kmers`

--- kmer.hs
+++ kmer.hs
@@ -4,10 +4,16 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Control.Applicative ((<$>), (<*>))
 import Rosalind
+
+kMers _ 0 = [[]]
+kMers alphabet n = (:) <$> alphabet <*> kMers alphabet (n - 1)
 
 main = do
     contents <- readFile "kmer.txt"
     let fasta = parse contents
     let dnas = snd $ unzip fasta
-    mapM_ putStrLn dnas
+    let dna = dnas !! 0
+    let alphabet = "ACGT"
+    mapM_ putStrLn $ kMers alphabet 4
