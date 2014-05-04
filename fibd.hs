6ee788e0f8570c56b7d32e2a767a25aa680f854c
codeguru <codeguru@users.sourceforge.net>
2014-05-03 19:26:46 -0600

    FIBD: Implement HUnit test

--- fibd.hs
+++ fibd.hs
@@ -4,11 +4,14 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Test.HUnit.Base ((~=?), (~:))
+import Test.HUnit.Text (runTestTT)
+
 fibd :: Int -> Int -> Int
 fibd n m = undefined
 
+testFibd = "Test fibd" ~: expected ~=? actual
+    where expected = [1, 1, 2, 2, 3, 4, 5, 7]
+          actual = map (\n -> fibd n 3) [1..8]
 
-main = do
-    print $ fibd n m
-    where n = 6
-          m = 3
+main = runTestTT testFibd
