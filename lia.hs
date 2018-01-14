82f7de7d38b1a0f3038f2aee32253f16d9517707
Code Apprentice <codeguru42@gmail.com>
2018-01-14 15:39:20 -0700

    LIA: Implement a recursive function to calculate the probability distribution after n generations

--- lia.hs
+++ lia.hs
@@ -4,4 +4,22 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
-main = undefined
+probAA = [0.5 , 0.5, 0.0 ]
+probAa = [0.25, 0.5, 0.25]
+probaa = [0.0 , 0.5, 0.5 ]
+
+multProbs x = map (x*)
+sumProbs = foldr (\[a, b, c] [a', b', c'] -> [a + a', b + b', c + c'])  [0.0, 0.0, 0.0]
+
+pdf 1 probs = probs
+pdf n probs = sumProbs $ zipWith multProbs probs pdf'
+    where pdfAA = pdf (n-1) probAA
+          pdfAa = pdf (n-1) probAa
+          pdfaa = pdf (n-1) probaa
+          pdf' = [pdfAA, pdfAa, pdfaa]
+
+main = do
+    print $ pdf 1 probAa
+    print $ pdf 2 probAa
+    print $ pdf 3 probAa
+    print $ map (flip pdf $ probAa) [1..10]
