88165f602c5266bdb4aeaca59a76723fbc37dbaf
Code Apprentice <codeguru42@gmail.com>
2018-01-20 04:52:22 -0700

    KMP: Test and implement `longestPrefix`

--- kmp.hs
+++ kmp.hs
@@ -4,9 +4,21 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Test.HUnit
 import Rosalind
 
+tests = test [testLongestPrefix]
+longestPrefixTestCases = [(2, "CAG", "CAT")]
+testLongestPrefix = map (\(expected, a, b)
+                            -> expected ~=? longestPrefix a b)
+                        longestPrefixTestCases
+
+longestPrefix (x:xs) (y:ys)
+    | x == y = longestPrefix xs ys + 1
+    | otherwise = 0
+
 main = do
+    runTestTT tests
     contents <- readFile "kmp.txt"
     let fasta = parse contents
     let dnas = snd $ unzip fasta
