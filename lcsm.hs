2a4a4bc4f69859ed7f33c314c7f9bc3d4e71b67c
Code-Apprentice <codeguru42@gmail.com>
2021-05-09 23:23:17 -0600

    Test for makeSuffixTree

--- lcsm.hs
+++ lcsm.hs
@@ -4,14 +4,29 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
+import Test.HUnit
 import Rosalind
+
+testMakeSuffixTree = "Test makeSuffixTree" ~: expected ~=? actual
+    where expected = Main.Node [
+              ("bxac", Main.Leaf 3),
+              ("c", Main.Leaf 6),
+              ("a", Main.Node [("c", Main.Leaf 5), ("bxac", Main.Leaf 2)]),
+              ("xa", Main.Node[("c", Main.Leaf 4), ("bxac", Main.Leaf 1)])
+            ]
+          actual = makeSuffixTree "xabxac"
 
 data SuffixTree = Leaf Int
     | Node [(String, SuffixTree)]
-    deriving Show
+    deriving (Show, Eq)
+
+makeSuffixTree :: String -> SuffixTree
+makeSuffixTree = undefined
 
 main = do
+    runTestTT testMakeSuffixTree
     contents <- readFile "lcsm.txt"
     let fasta = parse contents
     let dnas = snd $ unzip fasta
-    mapM_ putStrLn dnas
+    let suffixTree = makeSuffixTree (dnas !! 0)
+    putStrLn $ show suffixTree
