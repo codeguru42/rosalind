853c6b2332b0862b6cdec6b311a2de641d8f4334
Code-Apprentice <codeguru42@gmail.com>
2021-05-09 23:30:58 -0600

    LONG: Stub

---
+++ long.hs
@@ -0,0 +1,13 @@
+-- This program is free software. It comes without any warranty, to
+-- the extent permitted by applicable law. You can redistribute it
+-- and/or modify it under the terms of the Do What The Fuck You Want
+-- To Public License, Version 2, as published by Sam Hocevar. See
+-- http://sam.zoy.org/wtfpl/COPYING for more details.
+
+import Rosalind
+
+main = do
+    contents <- readFile "long.txt"
+    let fasta = parse contents
+    let dnas = snd $ unzip fasta
+    mapM_ putStrLn dnas
