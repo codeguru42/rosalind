e42269db9494c364fe96693d818281355d6664e2
Code-Apprentice <codeguru42@gmail.com>
2022-07-14 22:36:51 -0600

    Correctly parse nucleotide with line breaks

--- lcsm.py
+++ lcsm.py
@@ -2,9 +2,15 @@ import io
 
 
 def parse_fasta(fasta_file):
+    nucleotide = ''
     for line in fasta_file:
-        if line[0] != '>':
-            yield line.strip()
+        if line[0] == '>':
+            if nucleotide != '':
+                yield nucleotide
+                nucleotide = ''
+        else:
+            nucleotide += line.strip()
+    yield nucleotide
 
 
 fasta = '''>Rosalind_1
@@ -15,6 +21,17 @@ TAGACCA
 ATACA
 '''
 
+fasta2 = '''>Rosalind_1
+GAT
+TACA
+>Rosalind_2
+TAG
+ACCA
+>Rosalind_3
+ATA
+CA
+'''
+
 
 def test_parse_fasta():
     expected = [
@@ -23,6 +40,16 @@ def test_parse_fasta():
         'ATACA',
     ]
     dna = list(parse_fasta(io.StringIO(fasta)))
+    assert dna == expected
+
+
+def test_parse_fasta2():
+    expected = [
+        'GATTACA',
+        'TAGACCA',
+        'ATACA',
+    ]
+    dna = list(parse_fasta(io.StringIO(fasta2)))
     assert dna == expected
 
 
