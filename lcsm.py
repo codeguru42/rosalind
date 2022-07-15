36debba694186d4396481f5cb173d4bd7da8946e
Code-Apprentice <codeguru42@gmail.com>
2022-07-14 22:28:14 -0600

    Implement parse_fasta()

--- lcsm.py
+++ lcsm.py
@@ -1,3 +1,31 @@
+import io
+
+
+def parse_fasta(fasta_file):
+    for line in fasta_file:
+        if line[0] != '>':
+            yield line.strip()
+
+
+fasta = '''>Rosalind_1
+GATTACA
+>Rosalind_2
+TAGACCA
+>Rosalind_3
+ATACA
+'''
+
+
+def test_parse_fasta():
+    expected = [
+        'GATTACA',
+        'TAGACCA',
+        'ATACA',
+    ]
+    dna = list(parse_fasta(io.StringIO(fasta)))
+    assert dna == expected
+
+
 def main():
     pass
 
