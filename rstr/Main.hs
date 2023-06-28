4ee17be7c5846bdbc612621255f280e9a74b7d8b
Code-Apprentice <codeguru42@gmail.com>
2023-06-27 20:20:25 -0600

    RSTR: Solution

--- rstr/Main.hs
+++ rstr/Main.hs
@@ -1,10 +1,15 @@
 import System.Environment (getArgs)
 
+import Rosalind (logProb)
+
+rstr :: Int -> Double -> String -> Double
+rstr n x dna = 1 - lp' ^ n
+  where lp = logProb dna x
+        lp' = 1 - 10.0 ** lp
+
 main :: IO ()
 main = do
   nStr:xStr:dna:[] <- getArgs
   let n = read nStr :: Int
-  let x = read xStr :: Float
-  print n
-  print x
-  print dna
+  let x = read xStr :: Double
+  print $ rstr n x dna
