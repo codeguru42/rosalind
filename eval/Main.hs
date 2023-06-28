d2925f314b4a7da18af61bb48b5776822b012800
Code-Apprentice <codeguru42@gmail.com>
2023-06-27 23:55:50 -0600

    EVAL: Parse input file

--- eval/Main.hs
+++ eval/Main.hs
@@ -1,3 +1,13 @@
+import Data.List.Split (splitOn)
+import System.Environment (getArgs)
+
 main :: IO ()
 main = do
-  putStrLn "Hello, World"
+  path:[] <- getArgs
+  input <- readFile path
+  let nStr:dna:gcStr:rest = lines input
+  let n = read nStr :: Int
+  let gcs = map read $ splitOn " " gcStr :: [Double]
+  print n
+  print dna
+  print gcs
