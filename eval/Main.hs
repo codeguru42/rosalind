997173b63043e05b573d4daa8226949b39cbe41e
Code-Apprentice <codeguru42@gmail.com>
2023-06-30 21:50:47 -0600

    EVAL: Solution

--- eval/Main.hs
+++ eval/Main.hs
@@ -1,5 +1,11 @@
 import Data.List.Split (splitOn)
+import Rosalind (logProb)
 import System.Environment (getArgs)
+
+eval :: Int -> String -> [Double] -> [Double]
+eval n dna gcs = map (\x -> fromIntegral (n - m + 1) * 10**x) lps
+  where lps = map (logProb dna) gcs
+        m = length dna
 
 main :: IO ()
 main = do
@@ -8,6 +14,5 @@ main = do
   let nStr:dna:gcStr:_ = lines input
   let n = read nStr :: Int
   let gcs = map read $ splitOn " " gcStr :: [Double]
-  print n
-  print dna
-  putStrLn $ (unwords . map show) gcs
+  let probs = eval n dna gcs
+  putStrLn $ (unwords . map show) probs
