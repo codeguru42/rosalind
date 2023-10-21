a4db737296329a2b9ee8047228475efa09f9c16d
Code-Apprentice <codeguru42@gmail.com>
2023-10-21 12:12:26 -0600

    Double quoted strings

--- lcsm.py
+++ lcsm.py
@@ -2,9 +2,9 @@ from rosalind import parse_fasta
 
 
 def main():
-    with open('lcsm.txt') as file:
+    with open("lcsm.txt") as file:
         print(list(parse_fasta(file)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
