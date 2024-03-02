82096fc881ae74be4ed4e0f12d7d97db42c96b11
Code-Apprentice <codeguru42@gmail.com>
2024-03-02 15:27:13 -0700

    Formatting

--- rosalind.py
+++ rosalind.py
@@ -2,25 +2,25 @@ import io
 
 
 def parse_fasta(fasta_file):
-    nucleotide = ''
+    nucleotide = ""
     for line in fasta_file:
-        if line[0] == '>':
-            if nucleotide != '':
+        if line[0] == ">":
+            if nucleotide != "":
                 yield nucleotide
-                nucleotide = ''
+                nucleotide = ""
         else:
             nucleotide += line.strip()
     yield nucleotide
 
 
-fasta = '''>Rosalind_1
+fasta = """>Rosalind_1
 GATTACA
 >Rosalind_2
 TAGACCA
 >Rosalind_3
 ATACA
-'''
-fasta2 = '''>Rosalind_1
+"""
+fasta2 = """>Rosalind_1
 GAT
 TACA
 >Rosalind_2
@@ -29,14 +29,14 @@ ACCA
 >Rosalind_3
 ATA
 CA
-'''
+"""
 
 
 def test_parse_fasta():
     expected = [
-        'GATTACA',
-        'TAGACCA',
-        'ATACA',
+        "GATTACA",
+        "TAGACCA",
+        "ATACA",
     ]
     dna = list(parse_fasta(io.StringIO(fasta)))
     assert dna == expected
@@ -44,9 +44,9 @@ def test_parse_fasta():
 
 def test_parse_fasta2():
     expected = [
-        'GATTACA',
-        'TAGACCA',
-        'ATACA',
+        "GATTACA",
+        "TAGACCA",
+        "ATACA",
     ]
     dna = list(parse_fasta(io.StringIO(fasta2)))
     assert dna == expected
