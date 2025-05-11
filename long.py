e35210d2182e4a7fd5c760bd234e0ee201456582
Code-Apprentice <codeguru42@gmail.com>
2025-05-11 00:16:23 -0600

    Black formatting

--- long.py
+++ long.py
@@ -4,23 +4,23 @@ from rosalind import parse_fasta
 def prefixes(s):
     n = len(s)
     for i in range(n + 1):
-        yield s[:n-i]
+        yield s[: n - i]
 
 
 def test_prefixes():
-    result = list(prefixes('ATTAGACCTG'))
+    result = list(prefixes("ATTAGACCTG"))
     expected = [
-        'ATTAGACCTG',
-        'ATTAGACCT',
-        'ATTAGACC',
-        'ATTAGAC',
-        'ATTAGA',
-        'ATTAG',
-        'ATTA',
-        'ATT',
-        'AT',
-        'A',
-        '',
+        "ATTAGACCTG",
+        "ATTAGACCT",
+        "ATTAGACC",
+        "ATTAGAC",
+        "ATTAGA",
+        "ATTAG",
+        "ATTA",
+        "ATT",
+        "AT",
+        "A",
+        "",
     ]
     assert result == expected
 
@@ -32,32 +32,32 @@ def suffixes(s):
 
 
 def test_suffixes():
-    result = list(suffixes('ATTAGACCTG'))
+    result = list(suffixes("ATTAGACCTG"))
     expected = [
-        'ATTAGACCTG',
-        'TTAGACCTG',
-        'TAGACCTG',
-        'AGACCTG',
-        'GACCTG',
-        'ACCTG',
-        'CCTG',
-        'CTG',
-        'TG',
-        'G',
-        '',
+        "ATTAGACCTG",
+        "TTAGACCTG",
+        "TAGACCTG",
+        "AGACCTG",
+        "GACCTG",
+        "ACCTG",
+        "CCTG",
+        "CTG",
+        "TG",
+        "G",
+        "",
     ]
     assert result == expected
 
 
 def most_overlapping_pair(dnas):
-    max_a = ''
-    max_b = ''
-    max_overlap = ''
+    max_a = ""
+    max_b = ""
+    max_overlap = ""
     for a in dnas:
         for b in dnas:
             if a != b:
                 for suff in suffixes(a):
-                    pre = b[:len(suff)]
+                    pre = b[: len(suff)]
                     if suff == pre and len(pre) > len(max_overlap):
                         max_a = a
                         max_b = b
@@ -68,16 +68,16 @@ def most_overlapping_pair(dnas):
 
 def test_most_overlapping_pair():
     dnas = [
-        'ATTAGACCTG',
-        'CCTGCCGGAA',
-        'AGACCTGCCG',
-        'GCCGGAATAC',
+        "ATTAGACCTG",
+        "CCTGCCGGAA",
+        "AGACCTGCCG",
+        "GCCGGAATAC",
     ]
     result = most_overlapping_pair(dnas)
     expected = (
-        'ATTAGACCTG',
-        'AGACCTGCCG',
-        'AGACCTG',
+        "ATTAGACCTG",
+        "AGACCTGCCG",
+        "AGACCTG",
     )
     assert result == expected
 
@@ -88,19 +88,19 @@ def long(dnas):
         a, b, overlap = most_overlapping_pair(temp)
         temp.remove(a)
         temp.remove(b)
-        temp.append(a + b[len(overlap):])
+        temp.append(a + b[len(overlap) :])
     return temp[0]
 
 
 def test_long():
     dnas = [
-        'ATTAGACCTG',
-        'CCTGCCGGAA',
-        'AGACCTGCCG',
-        'GCCGGAATAC',
+        "ATTAGACCTG",
+        "CCTGCCGGAA",
+        "AGACCTGCCG",
+        "GCCGGAATAC",
     ]
     result = long(dnas)
-    expected = 'ATTAGACCTGCCGGAATAC'
+    expected = "ATTAGACCTGCCGGAATAC"
     assert result == expected
 
 
@@ -110,5 +110,5 @@ def main():
         print(long(list(fasta)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
