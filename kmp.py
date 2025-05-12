d0415abca96034b9e67f34c48da01c55c67c2119
Code-Apprentice <codeguru42@gmail.com>
2025-05-11 21:27:21 -0600

    KMP: Calculate failure array

--- kmp.py
+++ kmp.py
@@ -10,12 +10,20 @@ def suffixes(s: str) -> Iterable[str]:
         yield s[i:]
 
 
+def is_prefix(s: str, t: str) -> bool:
+    return s == t[: len(s)]
+
+
+def failure(s):
+    for k in range(1, len(s) + 1):
+        yield max((k - j for j in range(2, k) if is_prefix(s[j: k], s)), default=0)
+
+
 def main(filename: str):
     with open(filename) as f:
         fasta = parse_fasta(f)
-        for rna in fasta:
-            print(list(suffixes(rna)))
-
+        for dna in fasta:
+            print(list(failure(dna)))
 
 if __name__ == "__main__":
     typer.run(main)
