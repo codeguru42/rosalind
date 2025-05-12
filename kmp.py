ee11598f6a9db0ab7af85410bf85427b3fb2f8a2
Code-Apprentice <codeguru42@gmail.com>
2025-05-11 20:45:51 -0600

    KMP: Generate suffixes

--- kmp.py
+++ kmp.py
@@ -1,13 +1,21 @@
+from typing import Iterable
+
 import typer
 
 from rosalind import parse_fasta
 
 
+def suffixes(s: str) -> Iterable[str]:
+    for i in range(len(s)):
+        yield s[i:]
+
+
 def main(filename: str):
     with open(filename) as f:
         fasta = parse_fasta(f)
-        print(list(fasta))
+        for rna in fasta:
+            print(list(suffixes(rna)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     typer.run(main)
