601c81e46bb45360fd1724fda6c16e4c19e0d051
Code-Apprentice <codeguru42@gmail.com>
2025-05-19 00:04:32 -0600

    SPEC: Convert inputs to float

--- spec.py
+++ spec.py
@@ -1,9 +1,14 @@
 import typer
 
+
+def parse(f):
+    for line in f:
+        yield float(line)
+
 def main(filename: str):
     with open(filename, "r") as f:
-        for line in f:
-            print(line.strip())
+        l = list(parse(f))
+        print(l)
 
 if __name__ == '__main__':
     typer.run(main)
