29648f2ce6ee3b590724ca0378ebc01cd3175ac5
Code-Apprentice <codeguru42@gmail.com>
2025-11-14 16:47:15 -0700

    CORR: Parse fasta file

--- corr.py
+++ corr.py
@@ -1,6 +1,13 @@
-def main():
-    pass
+import typer
+
+from rosalind import parse_fasta
+
+
+def main(filename: str):
+    with open(filename) as f:
+        rnas = parse_fasta(f)
+        print(list(rnas))
 
 
 if __name__ == '__main__':
-    main()
+    typer.run(main)
