0fe61589605bc7fab8c51565892eb749d835d4df
Code-Apprentice <codeguru42@gmail.com>
2025-11-14 16:55:32 -0700

    CORR: Function to calculate hamming distance

--- corr.py
+++ corr.py
@@ -3,6 +3,14 @@ import typer
 from rosalind import parse_fasta
 
 
+def hamming(s1, s2):
+    d = 0
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            d += 1
+    return d
+
+
 def main(filename: str):
     with open(filename) as f:
         rnas = parse_fasta(f)
