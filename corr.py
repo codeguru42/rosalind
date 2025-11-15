b0fb15436714fde72f2504bd46cb8b5e9578110e
Code-Apprentice <codeguru42@gmail.com>
2025-11-15 09:03:35 -0700

    CORR: Attempting a solution

--- corr.py
+++ corr.py
@@ -3,7 +3,27 @@ import typer
 from rosalind import parse_fasta
 
 
-def hamming(s1, s2):
+def complement(rna: str) -> str:
+    def f():
+        for c in rna:
+            match c:
+                case 'A':
+                    yield 'T'
+                case 'C':
+                    yield 'G'
+                case 'G':
+                    yield 'C'
+                case 'T':
+                    yield 'A'
+                case _:
+                    raise f"Invalid nucleotide: {c}"
+    return "".join(f())
+
+
+def reverse_complement(rna: str) -> str:
+    return complement(rna[::-1])
+
+def hamming(s1: str, s2: str) -> int:
     d = 0
     for c1, c2 in zip(s1, s2):
         if c1 != c2:
@@ -13,8 +33,21 @@ def hamming(s1, s2):
 
 def main(filename: str):
     with open(filename) as f:
-        rnas = parse_fasta(f)
-        print(list(rnas))
+        rnas = list(parse_fasta(f))
+        for rna1 in rnas:
+            for rna2 in rnas:
+                d = hamming(rna1, rna2)
+                if d == 0:
+                    continue
+                if d == 1:
+                    print(f'{rna1}->{rna2}')
+                    continue
+                rc = reverse_complement(rna2)
+                d_comp = hamming(rna1, rc)
+                if d_comp == 0:
+                    continue
+                if d_comp == 1:
+                    print(f'{rna1}->{rc}')
 
 
 if __name__ == '__main__':
