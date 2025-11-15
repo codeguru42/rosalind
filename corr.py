02f8f37759afd13af71d397f42dbab45fc297d49
Code-Apprentice <codeguru42@gmail.com>
2025-11-15 11:26:46 -0700

    CORR: Formatting

--- corr.py
+++ corr.py
@@ -7,21 +7,23 @@ def complement(rna: str) -> str:
     def f():
         for c in rna:
             match c:
-                case 'A':
-                    yield 'T'
-                case 'C':
-                    yield 'G'
-                case 'G':
-                    yield 'C'
-                case 'T':
-                    yield 'A'
+                case "A":
+                    yield "T"
+                case "C":
+                    yield "G"
+                case "G":
+                    yield "C"
+                case "T":
+                    yield "A"
                 case _:
                     raise f"Invalid nucleotide: {c}"
+
     return "".join(f())
 
 
 def reverse_complement(rna: str) -> str:
     return complement(rna[::-1])
+
 
 def hamming(s1: str, s2: str) -> int:
     d = 0
@@ -40,15 +42,15 @@ def main(filename: str):
                 if d == 0:
                     continue
                 if d == 1:
-                    print(f'{rna1}->{rna2}')
+                    print(f"{rna1}->{rna2}")
                     continue
                 rc = reverse_complement(rna2)
                 d_comp = hamming(rna1, rc)
                 if d_comp == 0:
                     continue
                 if d_comp == 1:
-                    print(f'{rna1}->{rc}')
+                    print(f"{rna1}->{rc}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     typer.run(main)
