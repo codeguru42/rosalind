0142e6395ac491aaa56e4ff39b0312062db52568
Code-Apprentice <codeguru42@gmail.com>
2025-11-16 07:21:58 -0700

    DBRU: Initial solution

--- dbru.py
+++ dbru.py
@@ -2,12 +2,16 @@ from pathlib import Path
 
 import typer
 
+from rosalind import reverse_complement
 
 
 def main(filename: Path):
     with open(filename) as f:
         dnas = [line.strip() for line in f.readlines()]
-        print(dnas)
+        rcs = [reverse_complement(dna) for dna in dnas]
+        s = set(dnas) | set(rcs)
+        for kmer in sorted(s):
+            print(f"({kmer[:-1]}, {kmer[1:]})")
 
 
 if __name__ == "__main__":
